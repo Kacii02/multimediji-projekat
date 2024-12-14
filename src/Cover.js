e96bc73bec3e29c6c8f94dab30fb7d8b4e6d56f3
Kacii02 <nikolic.katarina002@gmail.com>
2024-12-14 16:03:28 +0100

    Prvi komit

---
+++ src/Cover.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Cover = () => {
+  return (
+    <div>
+      <img
+        src="https://www.cestujemtrekujem.com/wp-content/uploads/2018/11/P5138183_panorama_3-2.jpg"
+        alt="Cover photo"
+      />
+      <h1>Belgrade</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam nesciunt
+        minus sint atque eligendi, ipsa, exercitationem deserunt beatae debitis
+        facere iure, quasi pariatur repellendus ex dicta fugit architecto
+        distinctio itaque optio. Ipsam aliquid inventore nemo nam nostrum, ea
+        nulla excepturi possimus vel! Libero provident dolore optio voluptatibus
+        perferendis rerum ducimus?
+      </p>
+    </div>
+  );
+};
+
+export default Cover;
