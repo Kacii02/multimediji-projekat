455d53ffafb1c3856cef55e725cbc30e93729904
markoarsyc <markoarsyc@gmail.com>
2024-12-16 16:55:28 +0100

    Gotova baza slika

--- src/Cover.js
+++ src/Cover.js
@@ -4,7 +4,7 @@ import "./styles/Cover.css";
 const Cover = ({ collection }) => {
   return (
     <div className="cover">
-      <img src={collection.imgSrc} alt="Cover photo" />
+      <img src={collection.imgSrc || "https://cdn.pixabay.com/photo/2016/09/22/10/44/banner-1686943_1280.jpg"} />
       <h1>{collection.title}</h1>
       <p>{collection.description}</p>
     </div>
